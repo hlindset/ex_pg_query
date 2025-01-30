19a13bdaa2bec0159ad027165b4fa30f0d091a21
Håvard Lindset <havard@givn.no>
2025-01-30 13:02:04 +0100

    add coveralls

--- mix.exs
+++ mix.exs
@@ -7,7 +7,15 @@ defmodule ExPgQuery.MixProject do
       version: "0.1.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+        "coveralls.cobertura": :test
+      ]
     ]
   end
 
@@ -21,7 +29,8 @@ defmodule ExPgQuery.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:protox, "~> 1.7"}
+      {:protox, "~> 1.7"},
+      {:excoveralls, "~> 0.18", only: :test}
     ]
   end
 end
