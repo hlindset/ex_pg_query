89c5ef3ee8c41feea218e033cd748f069abbff40
Håvard Lindset <havard@givn.no>
2025-02-04 12:54:26 +0100

    test failure cases in fingerprint & normalize

--- test/ex_pg_query/normalize_test.exs
+++ test/ex_pg_query/normalize_test.exs
@@ -1,17 +1,24 @@
 defmodule ExPgQuery.NormalizeTest do
   use ExUnit.Case
 
+  alias ExPgQuery.Normalize
+
   doctest ExPgQuery.Normalize
 
   describe "normalize" do
     test "normalizes a simple query" do
-      {:ok, result} = ExPgQuery.Normalize.normalize("SELECT 1")
+      {:ok, result} = Normalize.normalize("SELECT 1")
       assert result == "SELECT $1"
+    end
+
+
+    test "returns error on invalid query" do
+      assert {:error, "syntax error at or near \"sellect\""} == Normalize.normalize("sellect 1")
     end
 
     test "normalizes IN(...)" do
       {:ok, result} =
-        ExPgQuery.Normalize.normalize(
+        Normalize.normalize(
           "SELECT 1 FROM x WHERE y = 12561 AND z = '124' AND b IN (1, 2, 3)"
         )
 
@@ -20,7 +27,7 @@ defmodule ExPgQuery.NormalizeTest do
 
     test "normalizes subselects" do
       {:ok, result} =
-        ExPgQuery.Normalize.normalize(
+        Normalize.normalize(
           "SELECT 1 FROM x WHERE y = (SELECT 123 FROM a WHERE z = 'bla')"
         )
 
@@ -28,63 +35,63 @@ defmodule ExPgQuery.NormalizeTest do
     end
 
     test "normalizes ANY(array[...])" do
-      {:ok, result} = ExPgQuery.Normalize.normalize("SELECT * FROM x WHERE y = ANY(array[1, 2])")
+      {:ok, result} = Normalize.normalize("SELECT * FROM x WHERE y = ANY(array[1, 2])")
       assert result == "SELECT * FROM x WHERE y = ANY(array[$1, $2])"
     end
 
     test "normalizes ANY(query)" do
-      {:ok, result} = ExPgQuery.Normalize.normalize("SELECT * FROM x WHERE y = ANY(SELECT 1)")
+      {:ok, result} = Normalize.normalize("SELECT * FROM x WHERE y = ANY(SELECT 1)")
       assert result == "SELECT * FROM x WHERE y = ANY(SELECT $1)"
     end
 
     test "works with complicated strings" do
-      {:ok, result} = ExPgQuery.Normalize.normalize("SELECT U&'d\\0061t\\+000061' FROM x")
+      {:ok, result} = Normalize.normalize("SELECT U&'d\\0061t\\+000061' FROM x")
       assert result == "SELECT $1 FROM x"
 
-      {:ok, result} = ExPgQuery.Normalize.normalize("SELECT u&'d\\0061t\\+000061'    FROM x")
+      {:ok, result} = Normalize.normalize("SELECT u&'d\\0061t\\+000061'    FROM x")
       assert result == "SELECT $1    FROM x"
 
       {:ok, result} =
-        ExPgQuery.Normalize.normalize("SELECT * FROM x WHERE z NOT LIKE E'abc'AND TRUE")
+        Normalize.normalize("SELECT * FROM x WHERE z NOT LIKE E'abc'AND TRUE")
 
       assert result == "SELECT * FROM x WHERE z NOT LIKE $1AND $2"
 
       {:ok, result} =
-        ExPgQuery.Normalize.normalize("SELECT U&'d\\0061t\\+000061'-- comment\nFROM x")
+        Normalize.normalize("SELECT U&'d\\0061t\\+000061'-- comment\nFROM x")
 
       assert result == "SELECT $1-- comment\nFROM x"
     end
 
     test "normalizes COPY" do
       {:ok, result} =
-        ExPgQuery.Normalize.normalize("COPY (SELECT * FROM t WHERE id IN ('1', '2')) TO STDOUT")
+        Normalize.normalize("COPY (SELECT * FROM t WHERE id IN ('1', '2')) TO STDOUT")
 
       assert result == "COPY (SELECT * FROM t WHERE id IN ($1, $2)) TO STDOUT"
     end
 
     test "normalizes SETs" do
-      {:ok, result} = ExPgQuery.Normalize.normalize("SET test=123")
+      {:ok, result} = Normalize.normalize("SET test=123")
       assert result == "SET test=$1"
     end
 
     test "normalizes weird SETs" do
-      {:ok, result} = ExPgQuery.Normalize.normalize("SET CLIENT_ENCODING = UTF8")
+      {:ok, result} = Normalize.normalize("SET CLIENT_ENCODING = UTF8")
       assert result == "SET CLIENT_ENCODING = $1"
     end
 
     test "does not fail if it does not understand parts of the statement" do
-      {:ok, result} = ExPgQuery.Normalize.normalize("DEALLOCATE bla; SELECT 1")
+      {:ok, result} = Normalize.normalize("DEALLOCATE bla; SELECT 1")
       assert result == "DEALLOCATE bla; SELECT $1"
     end
 
     test "normalizes EPXLAIN" do
-      {:ok, result} = ExPgQuery.Normalize.normalize("EXPLAIN SELECT x FROM y WHERE z = 1")
+      {:ok, result} = Normalize.normalize("EXPLAIN SELECT x FROM y WHERE z = 1")
       assert result == "EXPLAIN SELECT x FROM y WHERE z = $1"
     end
 
     test "normalizes DECLARE CURSOR" do
       {:ok, result} =
-        ExPgQuery.Normalize.normalize(
+        Normalize.normalize(
           "DECLARE cursor_b CURSOR FOR SELECT * FROM databases WHERE id = 23"
         )
 
